740d831c6bfdce8bf92f144feb7709411b945bc4
xevil-7 <xevil112677@gmail.com>
2026-07-04 11:27:23 +0530

    Create com/xevil7/kagex/MainActivity.kt

---
+++ android/app/src/main/kotlin/com/xevil7/kagex/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.xevil7.kagex
+
+import androidx.annotation.NonNull
+import libmtorrentserver.Libmtorrentserver
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.StandardMethodCodec
+import io.flutter.embedding.android.FlutterFragmentActivity
+import androidx.core.content.FileProvider
+import android.content.Intent
+import android.os.Build
+import android.net.Uri
+import java.io.File
+
+class MainActivity: FlutterFragmentActivity() {
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "com.xevil7.kagex.libmtorrentserver",
+            StandardMethodCodec.INSTANCE,
+            flutterEngine.dartExecutor.binaryMessenger.makeBackgroundTaskQueue()
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "start" -> {
+                    val config = call.argument<String>("config")
+                    try {
+                        val port = Libmtorrentserver.start(config)
+                        result.success(port)
+                    } catch (e: Exception) {
+                        result.error("ERROR", e.message, null)
+                    }
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "com.xevil7.kagex.apk_install",
+            StandardMethodCodec.INSTANCE,
+            flutterEngine.dartExecutor.binaryMessenger.makeBackgroundTaskQueue()
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "installApk" -> {
+                    val filePath = call.argument<String>("filePath")
+                    installApk(filePath)
+                    result.success(null)
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+
+    private fun installApk(filePath: String?) {
+        if (filePath == null) return
+        val file = File(filePath)
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        val apkUri: Uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            FileProvider.getUriForFile(this, "$packageName.fileprovider", file)
+        } else {
+            Uri.fromFile(file)
+        }
+        intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
+        startActivity(intent)
+    }
+}
